cc3b7f4beebdeea16c730dd76789e6083b20a510
Tommy Brosman <tbrosman@microsoft.com>
2024-09-20 11:11:17 -0700

    Another attempt at implementing BaseStore.

--- src/store/BaseStore.ts
+++ src/store/BaseStore.ts
@@ -1,13 +1,15 @@
 import { makeAutoObservable, runInAction } from "mobx"
 import type { SharedTreeConnection } from "../model/Model";
-import { Tree, type TreeNodeSchema, type TreeView } from "fluid-framework";
+import { ContainerSchema, type ImplicitFieldSchema, Tree, TreeConfiguration, type TreeNodeSchema, type TreeView } from "fluid-framework";
+import { TinyliciousClient } from "@fluidframework/tinylicious-client/internal";
 
-export class BaseStore<TState extends {}, TSchema extends TreeNodeSchema> {
+export class BaseStore<TState extends {}, TSchema extends ImplicitFieldSchema > {
 	private readonly state: TState;
 	private readonly sharedTreeConnection: SharedTreeConnection<TSchema>;
 
 	public constructor(
-		private readonly start: () => Promise<TreeView<TSchema>>,
+		// HACK: Only supports Tinylicious for now
+		private readonly client: TinyliciousClient,
 		private readonly applyFluidState: (treeView: TreeView<TSchema>, state: TState) => void,
 		preloadedState?: TState,
 		sharedTreeConnection?: SharedTreeConnection<TSchema>) {
@@ -28,8 +30,10 @@ export class BaseStore<TState extends {}, TSchema extends TreeNodeSchema> {
 	 * - Join or create a session
 	 * - Wire up events that dispatch reducers when the Shared Tree instance changes (either due to local or remote edits)
 	 */
-	public async connectToFluid(): Promise<void> {
-		const treeView = await this.start();
+	public async connectToFluid<TContainerSchema extends ContainerSchema>(
+		containerSchema: TContainerSchema,
+		treeConfiguration: TreeConfiguration): Promise<void> {
+		const treeView: TreeView<TSchema> = await this.start(containerSchema, treeConfiguration);
 		Tree.on(treeView.root, "treeChanged", () => {
 			runInAction(() => {
 				this.applyFluidState(treeView, this.state);
@@ -44,6 +48,46 @@ export class BaseStore<TState extends {}, TSchema extends TreeNodeSchema> {
 			this.applyFluidState(treeView, this.state);
 		});
 	}
+
+	/**
+	 * Join or start a Shared Tree session.
+	 * @returns The Tree View.
+	 */
+	private async start<TContainerSchema extends ContainerSchema>(
+		containerSchema: TContainerSchema,
+		treeConfiguration: TreeConfiguration):
+		Promise<TreeView<TSchema>> {
+		let treeView: TreeView<TSchema> | undefined;
+		if (location.hash) {
+			treeView = await this.loadExistingTreeView(location.hash.substring(1), containerSchema, treeConfiguration);
+		} else {
+			const result = await this.createNewTreeView(containerSchema, treeConfiguration);
+			location.hash = result.id;
+			treeView = result.pixelEditorTreeView;
+		}
+
+		return treeView;
+	}
+
+	private async createNewTreeView<TContainerSchema extends ContainerSchema>(
+		containerSchema: TContainerSchema,
+		treeConfiguration: TreeConfiguration):
+		Promise<{id: string, pixelEditorTreeView: TreeView<TSchema>}> {
+		const { container } = await this.client.createContainer(containerSchema);
+		const treeView = container.initialObjects.pixelEditorTree.schematize(treeConfiguration);
+		const id = await container.attach();
+		return { id, pixelEditorTreeView: treeView };
+	}
+
+	private async loadExistingTreeView<TContainerSchema extends ContainerSchema>(
+		id: string,
+		containerSchema: TContainerSchema,
+		treeConfiguration: TreeConfiguration):
+		Promise<TreeView<TSchema>> {
+		const { container } = await this.client.getContainer(id, containerSchema);
+		const treeView = container.initialObjects.pixelEditorTree.schematize(treeConfiguration);
+		return treeView;
+	}
 }
 
 /**
@@ -53,12 +97,11 @@ export class BaseStore<TState extends {}, TSchema extends TreeNodeSchema> {
  * @returns The composed store.
  */
 export async function setupStore<TState extends {}, TSchema extends TreeNodeSchema>(
-	start: () => Promise<TreeView<TSchema>>,
 	applyFluidState: (treeView: TreeView<TSchema>, state: TState) => void,
 	preloadedState?: TState,
 	sharedTreeConnection?: SharedTreeConnection<TSchema>):
 	Promise<BaseStore<TState, TSchema>> {
-	const store = new BaseStore(start, applyFluidState, preloadedState, sharedTreeConnection);
+	const store = new BaseStore(applyFluidState, preloadedState, sharedTreeConnection);
 
 	// Don't connect to Fluid if preloadedState is specified
 	if (preloadedState === undefined) {
